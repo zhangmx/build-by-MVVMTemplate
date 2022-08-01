a8925096a980e744d724d7dfa7fd20d40874106b
mingxing <icediv@163.com>
2022-08-01 17:07:58 +0800

    ignore com osachy doctor

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@ plugins {
     id("org.sonarqube") version app.web.drjackycv.buildsrc.Depends.Versions.sonarqubeVersion
     detekt
     id("com.github.ben-manes.versions") version app.web.drjackycv.buildsrc.Depends.Versions.checkDependencyVersionsVersion
-    id("com.osacky.doctor") version app.web.drjackycv.buildsrc.Depends.Versions.gradleDoctorVersion
+//    id("com.osacky.doctor") version app.web.drjackycv.buildsrc.Depends.Versions.gradleDoctorVersion
     id("com.autonomousapps.dependency-analysis") version app.web.drjackycv.buildsrc.Depends.Versions.dependencyAnalysisVersion
 }
 
@@ -35,14 +35,14 @@ allprojects {
     }
 }
 
-configure<com.osacky.doctor.DoctorExtension> {
-    disallowMultipleDaemons.set(false)
-    negativeAvoidanceThreshold.set(500)
-    warnWhenJetifierEnabled.set(true)
-
-    javaHome {
-        ensureJavaHomeIsSet.set(true)
-        ensureJavaHomeMatches.set(true)
-        failOnError.set(true)
-    }
-}
+//configure<com.osacky.doctor.DoctorExtension> {
+//    disallowMultipleDaemons.set(false)
+//    negativeAvoidanceThreshold.set(500)
+//    warnWhenJetifierEnabled.set(true)
+//
+//    javaHome {
+//        ensureJavaHomeIsSet.set(true)
+//        ensureJavaHomeMatches.set(true)
+//        failOnError.set(true)
+//    }
+//}
